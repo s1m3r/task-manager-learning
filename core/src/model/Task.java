124150e5532b2d616ae4cb54b9435160ff037098
sasha <dsunndusn@g,ail.cim>
2026-04-12 15:17:47 +0300

    Add Task model with fields and basic methods

--- core/src/model/Task.java
+++ core/src/model/Task.java
@@ -1,4 +1,37 @@
 package model;
 
 public class Task {
+    private String name;
+    private int priority;
+    private int id;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Task(String name, int priority, int id) {
+        this.name = name;
+        this.priority = priority;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
